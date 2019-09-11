fd9fdebb8325a9b52facfc729ac96c7589d0a7dc
gourabxz <gourabxz@gmail.com>
2019-09-12 00:59:41 +0600

    updates

--- scripts/main0.6.js
+++ scripts/main0.6.js
@@ -1,16 +1,35 @@
-var dynamicFunction = function(){
-  var page = document.getElementById("dynamicContent");
-  page.innerHTML = "Hello I am dynamic v3";
-}
 window.onload = () => {
     'use strict';
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
                .register('./sw0.2.js');
     }
-
+    
+    var dynamicFunction = function(){
+      var page = document.getElementById("dynamicContent");
+      var currentVersion = window.localStorage.getItem("versionCount");
+      page.innerHTML = "Hello I am dynamic v" + currentVersion;
+    }
     window.localStorage.setItem("compFunction", dynamicFunction.toString());
-    document.getElementById("vp").innerHTML = "V3"
+    
+    if(window.localStorage.getItem("versionCount") === null){
+      document.getElementById("vp").innerHTML = "V0";
+    }else{
+      document.getElementById("vp").innerHTML = "V" + window.localStorage.getItem("versionCount");
+    }
+
+    var vadd = document.getElementById("vAdd");
+    vadd.addEventListener("click", function(){
+      if(window.localStorage.getItem("versionCount") === null){
+        window.localStorage.setItem("versionCount", 1);
+      }else{
+        var currentVersion = window.localStorage.getItem("versionCount");
+        currentVersion++;
+        window.localStorage.setItem("versionCount", currentVersion);
+        document.getElementById("vp").innerHTML = "V" + window.localStorage.getItem("versionCount");
+      }
+    })
+    
   }
   var checkbox = document.querySelector("ion-checkbox");
   var calButton =  document.getElementById("calculate");
